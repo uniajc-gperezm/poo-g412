6cd2aac6d5ad0604b706b19aaa097c365fd4d720
Williamvilla25 <williamvilla727@gmail.com>
2026-09-15 23:49:02 -0500

    Agregando registro de fecha de nacimiento en Main de personas y finalizando el proceso

--- src/main/java/com/uniajc/Main.java
+++ src/main/java/com/uniajc/Main.java
@@ -1,24 +1,36 @@
 package com.uniajc;
 
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
 
+        System.out.println("-----------------------------------");
+        LocalDate fecha1 = LocalDate.of(1996, 5, 10);
         Persona persona1 = new Persona("123456789", "Juan Perez", 30);
         persona1.setIdentificacion("080990900");
 
         System.out.println("Imprimiendo información de la persona 1: ");
-        System.out.println(persona1);
+        persona1.mostrarInformacion();
+
+        System.out.println("-----------------------------------");
 
         Persona persona2 = new Persona("987654321");
         persona2.setNombre("Maria Gomez");
+        persona2.setFechaNacimiento(LocalDate.of(2006, 8, 20));
 
         System.out.println("Imprimiendo información de la persona 2: ");
-        System.out.println("Identificación: " + persona2.getIdentificacion() + ", Nombre: " + persona2.getNombre() + ", Edad: " + persona2.getEdad());
+        System.out.println("Identificación: " + persona2.getIdentificacion() + 
+                           ", Nombre: " + persona2.getNombre() + 
+                           ", Fecha Nac: " + persona2.getFechaNacimiento() +
+                           ", Edad: " + persona2.getEdad());
         
+        System.out.println("-----------------------------------");
+
         Persona persona3 = new Persona();
         persona3.setIdentificacion("123456789");
         persona3.setNombre("Carlos Rodriguez");
-        persona3.setEdad(25);
+        persona3.setFechaNacimiento(LocalDate.of(2001, 2, 15));
 
         System.out.println("Imprimiendo información de la persona 3: ");
         persona3.mostrarInformacion();
--- src/main/java/com/uniajc/MainEstudiante.java
+++ src/main/java/com/uniajc/MainEstudiante.java
@@ -2,21 +2,28 @@ package com.uniajc;
 
 public class MainEstudiante {
     public static void main(String[] args) {
+
+        System.out.println("-----------------------------------");
+
         Estudiante estudiante1 = new Estudiante("William", 21, "1109185394", "Ingeniería de Software", 4.5);
 
-        System.out.println("Imprimiendo información del estudiante: ");
+        System.out.println("Imprimiendo información del estudiante 1: ");
         estudiante1.mostrarInformacion();
         System.out.println(estudiante1.estudiar());
 
+        System.out.println("-----------------------------------");
+
         Estudiante estudiante2 = new Estudiante("Maria", 20, "3364852415", "Ingeniería de Software", 3.5);
 
-        System.out.println("Imprimiendo información del estudiante: ");
+        System.out.println("Imprimiendo información del estudiante 2: ");
         estudiante2.mostrarInformacion();
         System.out.println(estudiante2.estudiar());
 
+        System.out.println("-----------------------------------");
+
         Estudiante estudiante3 = new Estudiante("Pablo", 29, "1103183295", "Ingeniería de Software", 4.2);
 
-        System.out.println("Imprimiendo información del estudiante: ");
+        System.out.println("Imprimiendo información del estudiante 3: ");
         estudiante3.mostrarInformacion();
         System.out.println(estudiante3.estudiar());
     }
