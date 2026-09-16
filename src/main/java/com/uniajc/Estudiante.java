1b4b92589ff593207c8a8a36b7a00c6c5ee92ec0
Williamvilla25 <williamvilla727@gmail.com>
2026-09-15 22:27:54 -0500

    Creando los metodos Estudiante

--- src/main/java/com/uniajc/Estudiante.java
+++ src/main/java/com/uniajc/Estudiante.java
@@ -8,5 +8,19 @@ public class Estudiante {
     private String programaAcademico;
     private double promedioAcademico;
 
+    public Estudiante() {}
+
+    public Estudiante(String codigoEstudiantil) {
+        this.codigoEstudiantil = codigoEstudiantil;
+    }
+
+    public Estudiante(String nombre, int edad, String codigoEstudiantil, String programaAcademico, double promedioAcademico) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.codigoEstudiantil = codigoEstudiantil;
+        this.programaAcademico = programaAcademico;
+        this.promedioAcademico = promedioAcademico;
+    }
+    
     
 }
