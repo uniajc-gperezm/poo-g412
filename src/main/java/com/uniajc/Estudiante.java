1c0f61603d07e1578f71e0a91f331df26cf9bb4c
Williamvilla25 <williamvilla727@gmail.com>
2026-09-15 22:18:02 -0500

    Creando atributos de la clase Estudiante

--- src/main/java/com/uniajc/Estudiante.java
+++ src/main/java/com/uniajc/Estudiante.java
@@ -2,6 +2,11 @@ package com.uniajc;
 
 public class Estudiante {
 
-    
+    private String nombre;
+    private int edad;
+    private String codigoEstudiantil;
+    private String programaAcademico;
+    private double promedioAcademico;
+
     
 }
